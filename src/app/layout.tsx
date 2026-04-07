7d4e9aea5bc8c3f56e8c17f947f0dab61eaf794a
Clyde0513 <iclyde.villacrusis@gmail.com>
2026-04-07 16:43:40 -0700

    add favicon of qdc logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,10 @@ import Navbar from "../components/Navbar"
 export const metadata: Metadata = {
   title: "Quantum Device Consortium",
   description: "The Quantum Device Community aims to be an open association for pioneers of the quantum device design and simulation community.",
+  icons: {
+    icon: "/images/qdcLogo.png",
+    apple: "/images/qdcLogo.png",
+  },
 };
 
 export default function RootLayout({
